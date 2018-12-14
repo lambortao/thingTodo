edbf7b5927b36cd5eddf568eaa538c70f93f3eae
lambortao <lambortao@hotmail.com>
2018-12-14 09:51:03 +0800

    基础样式调试

--- app.js
+++ app.js
@@ -1,23 +1,8 @@
-// app.js
-Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-
-  },
-
-  /**
-   * 组件的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-
+App({
+  onLaunch: function () {
+    // 打开debug
+    wx.setEnableDebug({
+        enableDebug: false
+    });
   }
 })
